7efe2c9d0e6359eaedd20ba90f9b678bb236fa3d
JonasBa <jonas@badalic.com>
2025-05-07 20:42:16 -0700

    simplify

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react';
 import './App.css'
 import { useRef } from 'react';
+import { mat3 } from 'gl-matrix';
 
 const trace = [
   {
@@ -14,57 +15,16 @@ const trace = [
     duration: 50
   },
 ]
-
-
-type Mat3 = [number, number, number, number, number, number, number, number, number];
-function mat3mul(a: Mat3, b: Mat3): Mat3 {
-  return [
-    a[0] * b[0] + a[1] * b[3] + a[2] * b[6],
-    a[0] * b[1] + a[1] * b[4] + a[2] * b[7],
-    a[0] * b[2] + a[1] * b[5] + a[2] * b[8],
-
-    a[3] * b[0] + a[4] * b[3] + a[5] * b[6],
-    a[3] * b[1] + a[4] * b[4] + a[5] * b[7],
-    a[3] * b[2] + a[4] * b[5] + a[5] * b[8],
-
-    a[6] * b[0] + a[7] * b[3] + a[8] * b[6],
-    a[6] * b[1] + a[7] * b[4] + a[8] * b[7],
-    a[6] * b[2] + a[7] * b[5] + a[8] * b[8],
-  ]
-}
-
-function inverseMat3(a: Mat3): Mat3 | null { 
-  const det = a[0] * (a[4] * a[8] - a[5] * a[7]) - a[1] * (a[3] * a[8] - a[5] * a[6]) + a[2] * (a[3] * a[7] - a[4] * a[6]);
-  if(det === 0) return null;
-  const invDet = 1 / det;
-  return [
-    (a[4] * a[8] - a[5] * a[7]) * invDet,
-    (a[2] * a[7] - a[1] * a[8]) * invDet,
-    (a[1] * a[5] - a[2] * a[4]) * invDet,
-
-    (a[3] * a[8] - a[5] * a[6]) * invDet,
-    (a[0] * a[8] - a[2] * a[6]) * invDet,
-    (a[2] * a[3] - a[0] * a[5]) * invDet,
-
-    (a[3] * a[7] - a[4] * a[6]) * invDet,
-    (a[1] * a[6] - a[0] * a[7]) * invDet,
-    (a[0] * a[4] - a[1] * a[3]) * invDet,
-  ]
-}
-  
-
-
 class Flamegraph {
   canvas: HTMLCanvasElement | null;
   ctx: CanvasRenderingContext2D | null;
+  // view: x,y,width,height
+  view: [number,number,number,number]
+  trace: [number, number, number, number]
 
-  canvasMatrix: Mat3;
-  traceMatrix: Mat3;
-  viewMatrix: Mat3;
-  traceToCanvas: Mat3;
-  dprMatrix: Mat3;
-  timeToCanvasDPR: Mat3;
-
+  viewMatrix: mat3;
+  projectionMatrix: mat3;
+  mvpMatrix: mat3;
   constructor(canvas: HTMLCanvasElement) {
     this.canvas = canvas;
     this.ctx = canvas.getContext('2d');
@@ -75,57 +35,37 @@ class Flamegraph {
     canvas.style.width = '600px';
     canvas.style.height = '400px';
 
-    this.canvasMatrix = [
-      600, 0, 0,
-      0, 400, 0,
-      0, 0, 1,
-    ]
+    this.view = [0, 0, 100, 1];
+    this.trace = [0, 0, 100, 1];
 
-    this.traceMatrix = [
-      100, 0, 0,
-      0, 1, 0, 
-      0, 0, 1,
-    ]
+    this.viewMatrix = mat3.fromValues(
+      this.trace[2] / this.view[2], 0, 0,
+      0, this.trace[3] / this.view[3], 0,
+      this.view[0] * this.trace[2] / this.view[2], this.view[1] * this.trace[3] / this.view[3], 1,
+    );
 
-    this.viewMatrix = [
-      100, 0, 0,
-      0, 1, 0,
-      0, 0, 1,
-    ]
-
-    const viewToTrace = [
-      this.traceMatrix[0] * this.viewMatrix[0] / this.traceMatrix[0], 0, 0,
-      0, this.traceMatrix[1] * this.viewMatrix[1] / this.traceMatrix[1], 0,
-      0, 0, 1,
-    ]
-
-    this.traceToCanvas = [
-      this.canvasMatrix[0] / viewToTrace[0] , 0, 0,
-      0, 400, 0,
-      0, 0, 1,
-    ]
-
-    this.dprMatrix = [
-      window.devicePixelRatio, 0, 0,
+    this.projectionMatrix = mat3.fromValues(
+      600 / this.trace[2] * window.devicePixelRatio, 0, 0,
       0, window.devicePixelRatio, 0,
       0, 0, 1,
-    ]
+    );
 
-    this.timeToCanvasDPR = mat3mul(this.traceToCanvas, this.dprMatrix);
+    this.mvpMatrix = mat3.multiply(mat3.create(), this.projectionMatrix, this.viewMatrix);
     this.render();
   }
 
   getCursorPosition(x: number, y: number): [number, number] | null {
     if(!this.ctx || !this.canvas) return null;
 
-    const inverseCanvasMatrix = inverseMat3(this.timeToCanvasDPR);
-    
-    if(!inverseCanvasMatrix) return null;
-    const inverseSpace = mat3mul(inverseCanvasMatrix, this.dprMatrix);
+    // Convert to same coordinate system as the canvas
+    x *= window.devicePixelRatio;
+    y *= window.devicePixelRatio;
 
-    return [
-      (inverseSpace[0] * x + inverseSpace[1] * y + inverseSpace[2]),
-      (inverseSpace[3] * x + inverseSpace[4] * y + inverseSpace[5])
+    const unproject = mat3.invert(mat3.create(), this.mvpMatrix);
+
+    return[
+      (unproject[0] * x - unproject[6]),
+      (unproject[4] * y - unproject[7])
     ]
   }
   
@@ -139,10 +79,10 @@ class Flamegraph {
       const span = trace[i];
       this.ctx.fillStyle = i % 2 === 0 ? 'red' : 'blue';
 
-      const x = this.timeToCanvasDPR[0] * span.start;
-      const y = span.depth * 10 * this.dprMatrix[4];
-      const width = this.timeToCanvasDPR[0] * span.duration;
-      const height = 10 * this.dprMatrix[4];
+      const x = this.mvpMatrix[6] + this.mvpMatrix[0] * span.start;
+      const y = span.depth * 10 * this.mvpMatrix[4];
+      const width = this.mvpMatrix[0] * span.duration;
+      const height = 10 * this.mvpMatrix[4];
 
       this.ctx.fillRect(x, y, width, height);
     }
