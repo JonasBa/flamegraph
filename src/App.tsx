9c000ce8ba008ce1b18225919c0ccc8ddd39ebc0
JonasBa <jonas@badalic.com>
2025-05-07 18:58:29 -0700

    dpr

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,29 @@ import React from 'react';
 import './App.css'
 import { useRef } from 'react';
 
+const trace = [
+  {
+    start: 0,
+    duration: 400
+  },
+]
+
+
+function mat3mul(a: number[], b: number[]): number[] {
+  return [
+    a[0] * b[0] + a[1] * b[3] + a[2] * b[6],
+    a[0] * b[1] + a[1] * b[4] + a[2] * b[7],
+    a[0] * b[2] + a[1] * b[5] + a[2] * b[8],
+
+    a[3] * b[0] + a[4] * b[3] + a[5] * b[6],
+    a[3] * b[1] + a[4] * b[4] + a[5] * b[7],
+    a[3] * b[2] + a[4] * b[5] + a[5] * b[8],
+
+    a[6] * b[0] + a[7] * b[3] + a[8] * b[6],
+    a[6] * b[1] + a[7] * b[4] + a[8] * b[7],
+    a[6] * b[2] + a[7] * b[5] + a[8] * b[8],
+  ]
+}
 
 class Flamegraph {
   canvas: HTMLCanvasElement | null;
@@ -11,8 +34,11 @@ class Flamegraph {
     this.canvas = canvas;
     this.ctx = canvas.getContext('2d');
 
-    canvas.width = 600;
-    canvas.height = 400;
+    canvas.width = 600 * window.devicePixelRatio;
+    canvas.height = 400 * window.devicePixelRatio;
+
+    canvas.style.width = '600px';
+    canvas.style.height = '400px';
 
     this.render();
   }
@@ -26,36 +52,40 @@ class Flamegraph {
     if (!this.ctx || !this.canvas) {
       return;
     }
-
-    const canvas = [
+    
+    const canvasMatrix = [
       600, 0, 0,
       0, 400, 0,
       0, 0, 1,
     ]
 
-    const time = [
+    const traceMatrix = [
       100, 0, 0,
       0, 1, 0, 
       0, 0, 1,
     ]
 
     const timeToCanvas = [
-      canvas[0] / time[0] , 0, 0,
+      canvasMatrix[0] / traceMatrix[0] , 0, 0,
       0, 400, 0,
       0, 0, 1,
     ]
 
-    const rectangles = [
-      [0, 50]
+    const dprMatrix = [
+      window.devicePixelRatio, 0, 0,
+      0, window.devicePixelRatio, 0,
+      0, 0, 1,
     ]
 
-    for(const rect of rectangles) {
+    const timeToCanvasDPR = mat3mul(timeToCanvas, dprMatrix);
+
+    for(const span of trace) {
       this.ctx.fillStyle = 'red';
 
-      const x = timeToCanvas[0] * rect[0];
-      const width = timeToCanvas[0] * rect[1];
+      const x = timeToCanvasDPR[0] * span.start;
+      const width = timeToCanvasDPR[0] * span.duration;
 
-      this.ctx.fillRect(0, 0, width, 10);
+      this.ctx.fillRect(x, 0, width, 10 * dprMatrix[4]);
     }
   }
 }
